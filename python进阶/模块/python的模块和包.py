af41f0c7233999a16ef0d61fde0d719a55311921
mkcc <1182972594@qq.com>
2018-01-03 10:55:22 +0800

    2018-01-03,引用模块time，math

--- python进阶/模块/python的模块和包.py
+++ python进阶/模块/python的模块和包.py
@@ -1,5 +1,15 @@
 # !user.bin/python
+import math
+import time
 # 一个.py文件就是一个模块；引用其他模块用import 关键字
+def my_pow(x):
+    return math.sqrt(x)
+
+s = my_pow(9)
+print(s)
+
+time_01 = time.localtime()
+print(time.strftime('%Y-%m-%d %H:%M:%S', time_01))
 
 
 '''
