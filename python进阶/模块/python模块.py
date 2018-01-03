a6bb606e9e2ca754a700664a52fe994d535cbd42
mkcc <1182972594@qq.com>
2018-01-03 22:02:58 +0800

    2018-01-02

--- python进阶/模块/python模块.py
+++ python进阶/模块/python模块.py
@@ -1,3 +1,29 @@
 # !user/bin/python
+# 导入一个模块得部分函数
+from math import sin, sqrt
+import math, logging
+print(math.cos(3.1415/3))
+print(sqrt(9))
+print(sin(3.1415926/3))
+# 不同模块得同名函数调用，要加上模块名前缀
+print(math.log(2, 3))
+logging.log(10, 'message')
+'''
+# 导入一个模块得具体得某个函数，调用时不需要添加模块名.XXX,直接调用即可。
+from math import sin, sqrt
+print(sqrt(9))
+'''
+
+
+'''
+# 导入一整个模块函数
 import math
-math.sqrt()
+print(math.sqrt(9))
+'''
+
+'''
+1导入整个模块
+2、导入一个模块中得部分函数
+3、不同模块中得同名函数得调用，或者起别名
+'''
+
